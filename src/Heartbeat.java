e542baa228d9a0ef8e053d5a017806fa3f83af4c
finer <finer@172.27.69.128>
2018-01-17 16:48:44 -0600

    java class

---
+++ src/Heartbeat.java
@@ -0,0 +1,21 @@
+
+public class Heartbeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int time = 0;
+		
+		while (time<10) {
+			try {
+				Thread.sleep(1000);
+				time = time + 1;
+				System.out.println("Time elapsed: " + time);
+			}
+			catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
