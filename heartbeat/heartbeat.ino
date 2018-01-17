a98b1a676e43e66ae4d6228432114497d4a2e645
finer <finer@172.27.69.128>
2018-01-17 14:26:50 -0600

    heart

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,16 @@
+void setup(){
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+  delay(1000);
+  //int time = millis()/1000;
+  //Serial.print(time);
+  //Serial.println(" sec have elapsed");
+
+  digitalWrite(13, HIGH);       
+  delay(1000);             
+  digitalWrite(13, LOW);       
+}
+
